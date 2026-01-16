75ae1387167328371f0040bbef22a8b4afee73d5
Bogdan-uk <656olololol@gmail.com>
2026-01-16 23:00:16 +0200

    logger import fix

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
+
 import 'dotenv/config';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import helmet from 'helmet';
 import { errorHandler } from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+
+app.use(logger);
 app.use(express.json());
 app.use(cors());
-app.use(pino());
 app.use(helmet());
 
 app.use(notesRoutes);
