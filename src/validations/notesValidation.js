f888370bf51bb29eee4b3fc4c148997112936d36
Bogdan-uk <656olololol@gmail.com>
2026-01-25 22:53:45 +0200

    Hw

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -4,36 +4,35 @@ import { isValidObjectId } from 'mongoose';
 
 const objectIdValidator = (value, helpers) => {
   if (!isValidObjectId(value)) {
-    return helpers.message('not valid');
+    return helpers.message('Invalid noteId');
   }
   return value;
 };
+
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(...TAGS),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .optional(),
     search: Joi.string().allow(''),
   }),
 };
+
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
     content: Joi.string().allow(''),
-    tag: Joi.string().valid(...TAGS),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .optional(),
   }),
 };
 
 export const noteIdSchema = {
   [Segments.PARAMS]: Joi.object({
-    noteId: Joi.string()
-      .custom((value, helpers) => {
-        if (!isValidObjectId(value)) {
-          return helpers.message('This ID is not valid ');
-        }
-        return value;
-      })
-      .required(),
+    noteId: Joi.string().custom(objectIdValidator).required(),
   }),
 };
 
@@ -45,6 +44,8 @@ export const updateNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1),
     content: Joi.string().allow(''),
-    tag: Joi.string().valid(...TAGS),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .optional(),
   }).min(1),
 };
